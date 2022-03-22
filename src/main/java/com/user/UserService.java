8eaaf0dd959e2828720000332718ffe164ad4f0d
harshada <harshadasawant22@gmail.com>
2022-03-22 23:29:32 +0530

    first commit

---
+++ src/main/java/com/user/UserService.java
@@ -0,0 +1,13 @@
+package com.user;
+
+public class UserService {
+    public UserDao userDao;
+    public UserService(UserDao userDao){
+        this.userDao = userDao;
+    }
+
+    public int addUser(String name){
+        System.out.println("this is user = "+name);
+       return(userDao.save());
+    }
+}
